6b9599de41a27f1e0f718662ba1c5af4a744cb5a
OstrichRun <347647092@qq.com>
2015-04-17 17:19:30 +0800

    iOS添加Network

---
+++ src/iOS/do_Network_SM/ext_define/do_Network_ISM.h
@@ -0,0 +1,19 @@
+//
+//  do_Network_IMethod.h
+//  DoExt_SM
+//
+//  Created by @userName on @time.
+//  Copyright (c) 2015年 DoExt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol do_Network_ISM <NSObject>
+
+@required
+//实现同步或异步方法，parms中包含了所需用的属性
+- (void)getIP:(NSArray *)parms;
+- (void)getOperators:(NSArray *)parms;
+- (void)getStatus:(NSArray *)parms;
+
+@end
